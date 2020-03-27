426f445db9700a810b0e494d185491cc1a84e3e5
Jeth0214 <rolandjethrosuyom@yahoo.com>
2020-03-27 21:46:08 +0300

    continue functionality

--- drum.js
+++ drum.js
@@ -1,9 +1,10 @@
 // click to play
 const drumPad = document.querySelectorAll('.drum-pad');
 const clip = document.querySelectorAll('.clip');
-const mode = document.querySelector('#mode');
+const bankCheckBox = document.querySelector('#mode');
 const displayMessage = document.querySelector('#display');
 const volume = document.querySelector('#volume');
+const powerCheckedBox = document.querySelector('#power');
 
 
 //get data from json file
@@ -14,85 +15,108 @@ function getData() {
 }
 
 
-
-async function playSound(){
+//play Heater Kit
+async function playSound() {
     const data$ = await getData();
-    drumPad.forEach( el => {
-            data$.forEach( element => {
-                if(element.key.toUpperCase() === el.textContent){
-                    el.setAttribute("id" ,element.drumPadName[0]);
-                    el.children[0].setAttribute("src", element.src[0]);
-                    el.children[0].setAttribute("id", element.key.toUpperCase());
-                }
-        el.addEventListener('click', function(){
-            el.children[0].play();
-            displayMessage.textContent = el.id;
+    drumPad.forEach(el => {
+        data$.forEach(element => {
+            if (element.key.toUpperCase() === el.textContent) {
+                el.setAttribute("id", element.drumPadName[0]);
+                el.children[0].setAttribute("src", element.src[0]);
+                el.children[0].setAttribute("id", element.key.toUpperCase());
+            }
+            el.addEventListener('click', function () {
+                el.children[0].play();
+                displayMessage.textContent = el.id;
+            })
+
+
         })
-        
-        
-    })
     });
 }
 
-async function playSound2(){
+//play Smooth Piano Kit
+async function playSound2() {
     const data$ = await getData();
-    drumPad.forEach( el => {
-        
-            data$.forEach( element => {
-                if(element.key.toUpperCase() === el.textContent){
-                    el.setAttribute("id" ,element.drumPadName[1]);
-                    el.children[0].setAttribute("src", element.src[1]);
-                    el.children[0].setAttribute("id", element.key.toUpperCase());
+    drumPad.forEach(el => {
 
-                }
-        el.addEventListener('click', function(){
-            el.children[0].play();
+        data$.forEach(element => {
+            if (element.key.toUpperCase() === el.textContent) {
+                el.setAttribute("id", element.drumPadName[1]);
+                el.children[0].setAttribute("src", element.src[1]);
+                el.children[0].setAttribute("id", element.key.toUpperCase());
+
+            }
+            el.addEventListener('click', function () {
+                el.children[0].play();
+            })
+
+
         })
-        
-        
-    })
     });
 }
 
 
-// press a key 
+// toggle between Heater Kit and Smooth Piano KIt 
+bankCheckBox.addEventListener('change', pianoOrHeater);
 
-
-mode.addEventListener('change', test);
-
-function  test() {
+function pianoOrHeater() {
     if (mode.checked) {
         displayMessage.textContent = "Smooth Piano Kit";
-     playSound2()
+        playSound2()
     } else {
-        
-      playSound()
-      displayMessage.textContent = "Heater Kit";
+
+        playSound()
+        displayMessage.textContent = "Heater Kit";
     }
-  };
+};
 
-  volume.addEventListener('input', function () {
-    displayMessage.innerHTML = `<p> Volume ${volume.value} </p>`;
-  })
+//adjust the volume
+volume.addEventListener('input', function () {
+    displayMessage.innerHTML = `<p> Volume: ${volume.value} </p>`;
+    setTimeout(() => {
+        displayMessage.children[0].style.display = 'none';
+    }, 1000)
+})
 
-  
-  test();
 
 
-  document.onkeypress =  function(evt) {
+
+// play audio when a key was pressed
+document.onkeypress = function (evt) {
     evt = evt || window.event;
     var charCode = evt.keyCode || evt.which;
     var charStr = String.fromCharCode(charCode).toUpperCase();
-    clip.forEach ( audio => {
-        if(audio.id === charStr){
+    clip.forEach(audio => {
+        if (audio.id === charStr) {
             audio.parentElement.click();
             audio.parentElement.classList.add("button-active");
-        }else {
-            audio.parentElement.classList.remove("button-active");
+            setTimeout(() => {
+                audio.parentElement.classList.remove("button-active");
+            }, 100)
+
         }
     })
 };
 
+//power on and off
+powerCheckedBox.addEventListener('input', power);
+function power() {
+    if (!this.checked) {
+        pianoOrHeater();
+        console.log('unchecked')
+    } else {
+        console.log('checked')
+        bankCheckBox.disabled = true;
+        volume.disabled = true;
+        clip.forEach( audio => {
+            audio.currentTime = 0;
+            audio.pause();
+           
+        });
+        displayMessage.textContent = "";
+        
+    }
+}
 
-  // playSound()dqqqqq
-  // playSound2()ss
+power();
